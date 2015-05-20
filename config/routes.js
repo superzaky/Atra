d2744a193003f40f8821c93a7eb10a023ec59735
superzaky <superkytoz@gmail.com>
2015-05-20 12:37:24 +0200

    nog bezig met projects add

--- config/routes.js
+++ config/routes.js
@@ -5,11 +5,7 @@ module.exports =
 	{ 'resource' : '/login',      		'method' : 'post', 'controller' : 'auth',  		'action' : 'authorize',   'args' : { } },
 	{ 'resource' : '/logout',     		'method' : 'get',  'controller' : 'auth',  		'action' : 'unauthorize', 'args' : { } },
 	{ 'resource' : '/register',   		'method' : 'get',  'controller' : 'auth',  		'action' : 'register',    'args' : { } },
-<<<<<<< HEAD
 	{ 'resource' : '/projects', 		'method' : 'get',  'controller' : 'project',  	'action' : 'default',     'args' : { } },
-=======
-	{ 'resource' : '/projects', 	    'method' : 'get',  'controller' : 'project',  	'action' : 'default',     'args' : { } },
->>>>>>> 16eb3424b7d1eecb25ddde9a4f4b3ec0b21cdb1d
 	{ 'resource' : '/admin/chat', 		'method' : 'get',  'controller' : 'chat',  		'action' : 'default',     'args' : { } },
 	{ 'resource' : '/admin/game', 		'method' : 'get',  'controller' : 'game',  		'action' : 'default',     'args' : { } },
 	{ 'resource' : '/api/users',  		'method' : 'get',  'controller' : 'user',  		'action' : 'list',        'args' : { } },
--- controllers/project.js
+++ controllers/project.js
@@ -1,12 +1,6 @@
 module.exports =
 {
     default: function (req, res, args) {
-<<<<<<< HEAD
        res.render('projects');
-       //res.redirect('/projects');
-      // res.location('/projects');
-=======
-        res.render('projects', { 'user' : req.session.user });
->>>>>>> 16eb3424b7d1eecb25ddde9a4f4b3ec0b21cdb1d
     }
 }
