15b80abe847e1b86ef5ca4cdbdccda1533e24c90
AtraCaelus <atracaelus@gmail.com>
2015-07-08 00:41:45 +0200

    Refactor project controller and modal

--- src/app/_partials/modals/project/project-modal.controller.js
+++ src/app/_partials/modals/project/project-modal.controller.js
@@ -3,42 +3,49 @@
 
     var modals = angular.module('modals');
 
-    function ProjectModalCtrl ($scope, $modalInstance, Project, toastr, currentProject) {
-        $scope.instance = {
-            form: {
-                project: currentProject
-            },
+    function ProjectModalCtrl ($scope, $state, $modalInstance, Project, toastr) {
+        Project.current.selected = _.findWhere(Project.current.list, {_id: $state.params._id});
 
+        $scope.instance = {
             modal: {
                 close: function () {
                     $modalInstance.dismiss('cancel');
                 }
             },
 
+            form: {
+                project: angular.copy(Project.current.selected)
+            },
+
             Project: {
                 add: function () {
-                    var project = angular.copy($scope.instance.form.project);
-
-                    Project.save(project).$promise.then(function (response) {
-                        $modalInstance.close(angular.fromJson(angular.toJson(response)));
+                    Project.api.save($scope.instance.form.project).$promise
+                    .then(function (response) {
+                        Project.current.list.unshift(response);
                         toastr.success('Project has successfully been added', 'Success');
                     }, function (error) {
                         toastr.error(error.data, 'Error code ' + error.status);
+                    })
+                    .finally(function (response) {
+                        $modalInstance.close();
                     });
                 },
 
                 edit: function () {
-                    Project.save({_id: currentProject._id}, $scope.instance.form.project)
-                    .$promise.then(function (response) {
-                        $modalInstance.close(angular.fromJson(angular.toJson(response)));
+                    $scope.instance.form.project.$save()
+                    .then(function (response) {
+                        angular.extend(Project.current.selected, $scope.instance.form.project);
                         toastr.success('Project has successfully been modified', 'Success');
                     }, function (error) {
                         toastr.error(error.data, 'Error code ' + error.status);
+                    })
+                    .finally(function (response) {
+                        $modalInstance.close();
                     });
                 }
             }
         };
     }
 
-    modals.controller('ProjectModalCtrl', ['$scope', '$modalInstance', 'Project', 'toastr', 'currentProject', ProjectModalCtrl]);
+    modals.controller('ProjectModalCtrl', ['$scope', '$state', '$modalInstance', 'Project', 'toastr', ProjectModalCtrl]);
 })();
--- src/app/project/project.controller.js
+++ src/app/project/project.controller.js
@@ -4,35 +4,7 @@
     var project = angular.module('project');
 
     function ProjectCtrl ($rootScope, $scope, $state, $modal, toastr, Project, projects) {
-        projects.$promise.then(function (response) {
-            $scope.projects = angular.fromJson(angular.toJson(response));
-
-            $rootScope.$watch('toState', function (state) {
-                if (state.name === 'projects.add') {
-                    $modal.open({
-                        animation: false,
-                        templateUrl: '/app/_partials/modals/project/project-add-modal.html',
-                        controller: 'ProjectModalCtrl',
-                        resolve: { currentProject: function () { return null; }}
-                    })
-                    .result.then(function (response) { $scope.projects.unshift(response); })
-                    .finally(function() { $state.go('^'); });
-                } else if (state.name === 'projects.edit') {
-                    $modal.open({
-                        animation: false,
-                        templateUrl: '/app/_partials/modals/project/project-edit-modal.html',
-                        controller: 'ProjectModalCtrl',
-                        resolve: { currentProject: function () { return angular.copy(_.findWhere($scope.projects, {_id: $state.params._id})); }}
-                    })
-                    .result.then(function (response) {
-                        $scope.projects = _.each($scope.projects, function (project) {
-                            if (project._id === response._id) angular.extend(project, response);
-                        });
-                    })
-                    .finally(function() { $state.go('^'); });
-                }
-            });
-        });
+        $scope.projects = Project.current.list = projects;
 
         $scope.Project = {
             remove: function (_id) {
